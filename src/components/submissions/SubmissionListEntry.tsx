df230754dfc01c380c38f1e2a6660facf37327ec
Dev <strategytribe10@protonmail.com>
2022-07-12 16:32:21 -0400

    submission list entry layout fix

--- src/components/submissions/SubmissionListEntry.tsx
+++ src/components/submissions/SubmissionListEntry.tsx
@@ -23,9 +23,9 @@ export function SubmissionListEntry({
       onClick={() =>
         router.push(GoToSubmissionPage(bounty.id!, submission.id!))
       }
-      className="relative flex justify-between w-full items-center group"
+      className="relative w-full group grid grid-cols-4"
     >
-      <div className="flex flex-col items-start">
+      <div className="flex flex-col items-start col-span-2">
         <div className="flex gap-2">
           {bounty.tags?.map((tag) => {
             return (
@@ -36,7 +36,9 @@ export function SubmissionListEntry({
           })}
         </div>
 
-        <h5 className="title-xs group-hover:underline">{bounty.title}</h5>
+        <h5 className="title-xs group-hover:underline text-left">
+          {bounty.title}
+        </h5>
 
         <div className="bg-darker px-4 py-2 rounded absolute left-0 top-0 group-hover:visible invisible pointer-events-none group-hover:pointer-events-auto translate-x-12 -translate-y-8">
           Go to submission
@@ -45,7 +47,9 @@ export function SubmissionListEntry({
         <p className="text-unactive">{submission.answers.at(0)?.answer}...</p>
       </div>
 
-      <SubmissionStatus status={submission.state} />
+      <div className="place-self-center">
+        <SubmissionStatus status={submission.state} />
+      </div>
 
       <div className="flex flex-col items-end">
         <span className="title">{bounty.funds} ETH</span>
