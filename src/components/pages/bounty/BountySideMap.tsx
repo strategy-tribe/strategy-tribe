c6d0d57248ae8413793b9f2a6843dd64eb57d068
Dev <strategytribe10@protonmail.com>
2022-12-07 19:10:00 -0400

    chore: improved loading state for submissions
    in bounty page
    + hide submissions tab if user is not logged in

--- src/components/pages/bounty/BountySideMap.tsx
+++ src/components/pages/bounty/BountySideMap.tsx
@@ -1,10 +1,20 @@
 import { BountyView } from '@/lib/models/BountyView';
 
+import { useAuth } from '@/auth/AuthContext';
+
 import { useBountyContext } from './BountyContext';
 
 export function BountySideMap() {
   const { view, setView } = useBountyContext();
-  const pages = Object.entries(BountyView);
+  const { userId } = useAuth();
+
+  const pages = Object.entries(BountyView).filter((entry) => {
+    const showSubmissionsOnlyIfUserIsSignedIn = userId
+      ? true
+      : entry[1] !== BountyView.Submissions;
+
+    return showSubmissionsOnlyIfUserIsSignedIn;
+  });
 
   function goToPage(view: BountyView) {
     setView(view);
@@ -25,7 +35,7 @@ export function BountySideMap() {
             key={i}
             onClick={() => goToPage(value as BountyView)}
           >
-            {label}
+            {value === BountyView.Submissions ? 'Your submissions' : label}
           </button>
         );
       })}
