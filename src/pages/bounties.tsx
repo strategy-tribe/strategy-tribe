0dc6393daa3e576603d6f3a20c3f53e807fb9a34
Dev <strategytribe10@protonmail.com>
2022-12-08 22:13:07 -0400

    test: trying tRPC prefetch
    + installed sharp as next suggested

--- src/pages/bounties.tsx
+++ src/pages/bounties.tsx
@@ -1,23 +1,44 @@
+import { createProxySSGHelpers } from '@trpc/react-query/ssg';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import superjson from 'superjson';
 
 import { MapDataWithFeatures } from '@/lib/models/MapData';
 import { overcomeSerialization } from '@/lib/utils/overcomeSerialization';
 
 import AppLayout from '@/components/layouts/AppLayout';
 import { Explore } from '@/components/pages/explore/Explore';
+import { DEFAULT_FILTER } from '@/components/pages/explore/filters/utils/DefaultFilter';
 
+import { createContextInner } from '@/server/context';
 import prisma from '@/server/prisma/prismaClient';
 import { getMapData } from '@/server/routers/map';
+import { appRouter } from '@/server/routers/_app';
 
 import { NextPageWithLayout } from './_app';
 
 export const getStaticProps: GetStaticProps = async () => {
-  const mapData = await getMapData(prisma);
+  //We can't cache of all the possible filters, but we can cache the default one.
+  //The default one is defined.
+  //we can prefetch this with tRPC instructions: https://trpc.io/docs/ssg-helpers
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: await createContextInner(),
+    transformer: superjson, // optional - adds superjson serialization
+  });
 
+  await ssg.bounty.getBounties.prefetch({ ...DEFAULT_FILTER.query });
+
+  //#region Map data
+  const mapData = await getMapData(prisma);
   const parsedData = overcomeSerialization(mapData);
+  //#endregion Map Data
   return {
-    props: { mapData: parsedData },
+    props: {
+      trpcState: ssg.dehydrate(),
+      mapData: parsedData,
+    },
+    revalidate: 60 * 5, //every 5 minutes
   };
 };
 
