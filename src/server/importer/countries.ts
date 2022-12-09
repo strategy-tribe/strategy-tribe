5645942ed0dda24d23f0c9606be9f62181fd9be7
Dev <strategytribe10@protonmail.com>
2022-12-09 09:06:17 -0400

    fix: build fix

--- src/server/importer/countries.ts
+++ src/server/importer/countries.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from '@prisma/client';
 
 import { ReadFileInDataFolder } from '../routers/ReadFileInDataFolder';
 import { LOG } from './utils';
-import { ReadFileInDataFolder } from '../routers/ReadFileInDataFolder';
 
 /** Resets the countries in DB */
 export async function AddCountriesToDB(prisma: PrismaClient) {
