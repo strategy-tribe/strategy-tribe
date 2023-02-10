b7654aad6c09afc8d23d1e69e18ff7e035224f8d
johnm707 <116788039+johnm707@users.noreply.github.com>
2023-02-10 13:36:36 +0530

    fix: misc fixes (#49)

--- src/inngest/incrementSchedule.ts
+++ src/inngest/incrementSchedule.ts
@@ -7,7 +7,7 @@ const inngest = new Inngest({ name: 'StrategyTribe' });
 
 export default inngest.createScheduledFunction(
   'Bounty Increment', // The name of your function, used for observability.
-  '*/5 * * * *', // The cron syntax for the function.
+  '* */24 * * *', // The cron syntax for the function.
   // This code will be called on the schedule above
   async () => {
     LOG('Running bounty increment job');
