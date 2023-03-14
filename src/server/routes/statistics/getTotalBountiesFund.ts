7af161adee39b98858b42a986b5277f0fda344ed
neojune <neoojune@gmail.com>
2023-03-14 18:16:39 +0530

    feat: rebase

--- src/server/routes/statistics/getTotalBountiesFund.ts
+++ src/server/routes/statistics/getTotalBountiesFund.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from '@prisma/client';
-import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+
+import { calculateTotalBountyFund } from '@/lib/utils/statisticsHelpers';
+
+import { publicProcedure } from '@/server/procedures';
 
 import { ThenArg } from '../utils/helperTypes';
 
-export async function GetLastWeekTotalBountiesFundData(prisma: PrismaClient) {
-  const payoutStatus = await prisma.bounty.findMany({
+export async function _getLastWeekTotalBountiesFundData(prisma: PrismaClient) {
+  const fundData = await prisma.bounty.findMany({
     select: {
       slug: true,
       wallet: {
@@ -18,25 +22,21 @@ export async function GetLastWeekTotalBountiesFundData(prisma: PrismaClient) {
       updatedAt: 'desc',
     },
   });
-  return payoutStatus;
+  const totalBountyFunds = calculateTotalBountyFund(fundData, false);
+  return totalBountyFunds;
 }
 
-export const getTotalBountiesFund = async (
-  prisma: PrismaClient
-): Promise<LastWeekTotalBountyFundData> => {
-  const lastWeekTotalBountyFund = await GetLastWeekTotalBountiesFundData(
-    prisma
-  );
-  if (!lastWeekTotalBountyFund) {
-    throw new TRPCError({
-      code: 'INTERNAL_SERVER_ERROR',
-      message: 'Unable to query last week total bounty fund data',
-    });
-  }
-
-  return lastWeekTotalBountyFund;
-};
-
-export type LastWeekTotalBountyFundData = NonNullable<
-  ThenArg<ReturnType<typeof GetLastWeekTotalBountiesFundData>>
+export type TotalBountyFundData = NonNullable<
+  ThenArg<ReturnType<typeof _getLastWeekTotalBountiesFundData>>
 >;
+
+const GetSubmissionsDataSchema = z.object({}).optional();
+
+export const getTotalBountiesFund = publicProcedure
+  .input(GetSubmissionsDataSchema)
+  .query(async ({ input, ctx: { prisma } }) => {
+    const lastWeekTotalBountyFund = await _getLastWeekTotalBountiesFundData(
+      prisma
+    );
+    return { lastWeekTotalBountyFund, ...input };
+  });
