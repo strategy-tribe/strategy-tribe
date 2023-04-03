2395c85f476a592a1226991aa1651e623454bace
johnm707 <thejohnm707@gmail.com>
2023-04-04 02:07:41 +0530

    feat: referral system

--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -2,6 +2,7 @@ import { router } from '../procedures';
 import { getInfo } from '../routes/users/getInfo';
 import { getLeaderboardUsers } from '../routes/users/getLeaderboardUsers';
 import { getUser } from '../routes/users/getUser';
+import { postReferral } from '../routes/users/postreferral';
 import { updateUsername } from '../routes/users/updateUsername';
 
 export const userRouter = router({
@@ -9,4 +10,5 @@ export const userRouter = router({
   updateUsername: updateUsername,
   getUser: getUser,
   getInfo: getInfo,
+  postReferral: postReferral,
 });
--- src/server/routes/users/getUser.ts
+++ src/server/routes/users/getUser.ts
@@ -1,4 +1,5 @@
 import { ThenArg } from '@trpc/server';
+import { v4 as uuidv4 } from 'uuid';
 
 import { signedInOnlyProcedure } from '@/server/procedures';
 
@@ -6,14 +7,29 @@ export type User = NonNullable<ThenArg<ReturnType<typeof getUser>>>;
 
 export const getUser = signedInOnlyProcedure.query(
   async ({ ctx: { prisma, session } }) => {
-    const user = await prisma.user.findUnique({
+    let user = await prisma.user.findUnique({
       where: {
         address: session.user.address,
       },
       select: {
         username: true,
+        referralCode: true,
       },
     });
+    if (!user?.referralCode) {
+      user = await prisma.user.update({
+        where: {
+          address: session.user.address,
+        },
+        data: {
+          referralCode: uuidv4(),
+        },
+        select: {
+          username: true,
+          referralCode: true,
+        },
+      });
+    }
     return { user };
   }
 );
